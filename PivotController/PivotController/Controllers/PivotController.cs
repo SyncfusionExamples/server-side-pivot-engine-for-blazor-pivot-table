8e6fd71bb385c7785904ae747659cfe3eccdf479
Parthiban Palanivel <parthiban.palanivel@syncfusion.com>
2023-03-28 11:43:46 +0530

    feature(BLAZ-30177):sample and pivotcontroller for server engine added.

---
+++ PivotController/PivotController/Controllers/PivotController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Dynamic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using Newtonsoft.Json;
+using Syncfusion.EJ2.Pivot;
+
+namespace PivotController.Controllers
+{
+    [Route("api/[controller]")]
+    public class PivotController : Controller
+    {
+        private readonly IMemoryCache _cache;
+        private IWebHostEnvironment _hostingEnvironment;
+        private bool isRendered;
+        private PivotEngine<DataSource.PivotViewData> PivotEngine = new PivotEngine<DataSource.PivotViewData>();
+        JsonSerializerOptions customSerializeOptions = new JsonSerializerOptions()
+        {
+            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+            Converters =
+                {
+                    new SortedDictionaryConverter(),
+                    new DoubleConverter(),
+                    new ObjectToInferredTypesConverter()
+                }
+        };
+
+        public PivotController(IMemoryCache cache, IWebHostEnvironment environment)
+        {
+            _cache = cache;
+            _hostingEnvironment = environment;
+        }
+
+        [Route("/api/pivot/post")]
+        [HttpPost]
+        public async Task<object> Post([FromBody]object args)
+        {
+            FetchData param = JsonConvert.DeserializeObject<FetchData>(args.ToString());
+            if (param.Action == "fetchFieldMembers")
+            {
+                return await GetMembers(param);
+            }
+            else if (param.Action == "fetchRawData")
+            {
+                return await GetRawData(param);
+            }
+            else
+            {
+                return await GetPivotValues(param);
+            }
+        }
+        public async Task<EngineProperties> GetEngine(FetchData param)
+        {
+            isRendered = false;
+            return await _cache.GetOrCreateAsync("engine" + param.Hash,
+                async (cacheEntry) =>
+                {
+                    isRendered = true;
+                    cacheEntry.SetSize(1);
+                    cacheEntry.AbsoluteExpiration = DateTimeOffset.UtcNow.AddMinutes(60);
+                    PivotEngine.Data = await GetData(param);
+                    return await PivotEngine.GetEngine(param);
+                });
+        }
+
+        public async Task<object> GetData(FetchData param)
+        {
+            return await _cache.GetOrCreateAsync("dataSource" + param.Hash,
+                async (cacheEntry) =>
+                {
+                    cacheEntry.SetSize(1);
+                    cacheEntry.AbsoluteExpiration = DateTimeOffset.UtcNow.AddMinutes(60);
+
+                    // Here, you can refer different kinds of data sources. We've bound a collection in this illustration.
+                    // return new DataSource.UniversityData().ReadUniversityJSONData(_hostingEnvironment.ContentRootPath + "\\DataSource\\universitydata.json");
+                    return new DataSource.PivotViewData().GetVirtualData();
+
+                    //return new DataSource.UniversityData().ReadUniversityJSONData(_hostingEnvironment.ContentRootPath + "\\DataSource\\universitydata.json");
+                    // EXAMPLE:
+                    // Other data sources, such as DataTable, CSV, JSON, etc., can be bound as shown below.
+                    // return new DataSource.BusinessObjectsDataView().GetDataTable();
+                    // return new DataSource.PivotJSONData().ReadJSONData(_hostingEnvironment.ContentRootPath + "\\DataSource\\sales-analysis.json");
+                    // return new DataSource.PivotCSVData().ReadCSVData(_hostingEnvironment.ContentRootPath + "\\DataSource\\sales.csv");
+                    // return new DataSource.PivotJSONData().ReadJSONData("http://cdn.syncfusion.com/data/sales-analysis.json");
+                    // return new DataSource.PivotCSVData().ReadCSVData("http://cdn.syncfusion.com/data/sales-analysis.csv");
+                    // return new DataSource.PivotExpandoData().GetExpandoData();
+                    // return new DataSource.PivotDynamicData().GetDynamicData();
+                });
+        }
+
+        public async Task<object> GetMembers(FetchData param)
+        {
+            EngineProperties engine = await GetEngine(param);
+            Dictionary<string, object> returnValue = new Dictionary<string, object>();
+            returnValue["memberName"] = param.MemberName;
+            returnValue["members"] = Serialize(engine.FieldList[param.MemberName].DateMember, customSerializeOptions);
+            return returnValue;
+        }
+
+        public async Task<object> GetRawData(FetchData param)
+        {
+            EngineProperties engine = await GetEngine(param);
+            return PivotEngine.GetRawData(param, engine);
+        }
+
+        public async Task<object> GetPivotValues(FetchData param)
+        {
+            Stopwatch ss = new Stopwatch();
+            ss.Start();
+            EngineProperties engine = await GetEngine(param);
+            if (!isRendered)
+            {
+                engine = await PivotEngine.PerformAction(engine, param);
+            }
+            _cache.Remove("engine" + param.Hash);
+            _cache.Set("engine" + param.Hash, engine, new MemoryCacheEntryOptions().SetSize(1).SetAbsoluteExpiration(DateTimeOffset.UtcNow.AddMinutes(60)));
+            var pivotValues = PivotEngine.GetPivotValues();
+            ss.Stop();
+            Console.WriteLine(ss.ElapsedMilliseconds);
+            return pivotValues;
+        }
+
+        private string Serialize(dynamic fieldItem, JsonSerializerOptions jsonSerializerOptions = null)
+        {
+            string serializedString;
+            serializedString = fieldItem != null ? System.Text.Json.JsonSerializer.Serialize(fieldItem, jsonSerializerOptions ?? new JsonSerializerOptions { DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull }) : null;
+            return serializedString;
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # server-side-pivot-engine-for-blazor-pivot-table
-The server-side pivot engine can efficiently process raw data from different data sources and only communicate the necessary data to the view-port of the pivot table. This results in less bandwidth and more efficient performance.
+Introduced a server-side engine where all the pivot calculations, filtering, sorting, etc. are done. Then, the information to be displayed in the viewport will be passed to the client side. This prevents transferring the entire data source to a browser, which reduces network traffic and increases rendering performance of the Pivot Table especially where there are millions of data. It works best when virtual scrolling is enabled. The engine supports different kinds of data sources such as collections, JSON, CSV, DataTable, and dynamic types.
+
+The following steps demonstrate how to run the application,
+* Open the "PivotController" application in Visual Studio.
+* Dependent packages will be downloaded automatically from the nuget.org site.
+* Run the application once the packages are downloaded.
+* Once the application is hosted in the localhost, copy and paste its URL in the pivot table sample in the index.razor file of the blazor wasm application in Sample folder.
+* Then build and run the apllication. The pivot table will be populated in the browser.
